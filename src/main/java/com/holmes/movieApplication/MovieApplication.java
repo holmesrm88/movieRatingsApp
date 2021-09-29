5270213f4f937119d1e31472ed469a0c23fe9231
Matt Holmes <matt-macbook@matts-mbp-2.myfiosgateway.com>
2021-09-29 17:19:23 -0400

    first commit

---
+++ src/main/java/com/holmes/movieApplication/MovieApplication.java
@@ -0,0 +1,13 @@
+package com.holmes.movieApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/holmes/movieApplication/controller/movieController.java
@@ -0,0 +1,11 @@
+package com.holmes.movieApplication.controller;
+
+import lombok.extern.java.Log;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
+
+@ComponentScan
+@RestController
+@Log
+public class movieController {
+}
